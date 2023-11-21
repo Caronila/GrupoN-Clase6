54697e5a8df6b8b20d90808ba5b06abe2a64308b
Antonella <antomuttib@gmail.com>
2023-11-21 19:20:46 -0300

    Creación de evento

--- RegistroFormulario/src/components/Registro.jsx
+++ RegistroFormulario/src/components/Registro.jsx
@@ -1,28 +1,44 @@
 import React from 'react'
+import { useState } from 'react';
 import './Registro.css'
 
 const Registro = () => {
+    const [mensaje, setMensaje] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        let nombre = e.target.nombre.value
+        setMensaje(`${nombre}, tu formulario se envió correctamente`)
+        setEstiloMensaje('message-container')
+
+    }
     return (
-        <>   
-        
-        <main>
-                <form>
-                <h1>Completa el registro</h1>
-                <label htmlFor="nombre">Nombre:</label>
-                <input type="text" id="nombre" name="nombre" required />
-                <label htmlFor="apellido">Apellido:</label>
-                <input type="text" id="apellido" name="apellido" required />
-                <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="email" required />
-                <label htmlFor="telefono">Teléfono:</label>
-                <input type="tel" id="telefono" name="telefono" required />
-                <label htmlFor="password">Contraseña:</label>
-                <input type="password" id="password" name="password" required />
-                <label htmlFor="confirm-password">Confirmar Contraseña:</label>
-                <input type="password" id="confirm-password" name="confirm-password" required />
-                <button type="submit">Registrar</button>
-            </form>
+        <>
+
+            <main>
+                <form onSubmit={handleSubmit}>
+                    <h1>Completa el registro</h1>
+                    <label htmlFor="nombre">Nombre:</label>
+                    <input type="text" id="nombre" name="nombre" required />
+                    <label htmlFor="apellido">Apellido:</label>
+                    <input type="text" id="apellido" name="apellido" required />
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" id="email" name="email" required />
+                    <label htmlFor="telefono">Teléfono:</label>
+                    <input type="tel" id="telefono" name="telefono" required />
+                    <label htmlFor="password">Contraseña:</label>
+                    <input type="password" id="password" name="password" required />
+                    <label htmlFor="confirm-password">Confirmar Contraseña:</label>
+                    <input type="password" id="confirm-password" name="confirm-password" required />
+                    <button type="submit">Registrar</button>
+                </form>
+                <div>
+                    <p>{mensaje}</p>
+                </div>
+
             </main>
+
         </>
     )
 }
